08330b72a31efd3978054f78b78ec78526315837
CinLo <cinlos3382@gmail.com>
2023-01-31 09:36:59 -0300

    completando Dashboard

--- src/scenes/geography/index.js
+++ src/scenes/geography/index.js
@@ -1,8 +1,8 @@
-import { Box } from '@mui/material'
-import Header from '../../components/Header'
-import GeographyChart from '../../components/GeographyChart'
+import { Box } from '@mui/material';
+import Header from '../../components/Header';
+import GeographyChart from '../../components/GeographyChart';
 import { tokens } from '../../theme';
-import { useTheme } from '@mui/material'
+import { useTheme } from '@mui/material';
 
 
 const Geography = () => {
